df334a70694a0dce8843487978b19601a8daabf1
tdelaroche <theo.delaroche@edu.univ-fcomte.fr>
2026-02-11 11:28:51 +0100

    Added some tests for the constructors of IBinaryFile

--- testSerial.cc
+++ testSerial.cc
@@ -8,6 +8,54 @@ TEST(SerialTest, firstTest) {
 
 }
 
+// Constructor tests
+TEST(SerialOBinaryFileConstructorTest, BasicConstructorTest) {
+  const std::string filename = "test.txt";
+  {
+    serial::OBinaryFile file(filename);
+    constexpr uint32_t value = 123u;
+    file << value;
+  }
+  {
+    serial::IBinaryFile file(filename);
+    uint32_t result = 0;
+    file >> result;
+
+    EXPECT_EQ(result, 123u);
+  }
+}
+TEST(SerialOBinaryFileConstructorTest, MoveConstructorTest) {
+  {
+    serial::OBinaryFile f1("test.txt");
+    serial::OBinaryFile f2(std::move(f1));
+    constexpr uint32_t value = 123u;
+    f2 << value;
+  }
+  {
+    serial::IBinaryFile f2("test.txt");
+    uint32_t result = 0;
+    f2 >> result;
+
+    EXPECT_EQ(result, 123u);
+  }
+}
+TEST(SerialOBinaryFileConstructorTest, MoveAssignmentTest) {
+  {
+    serial::OBinaryFile f1("test.txt");
+    serial::OBinaryFile f2 = std::move(f1);
+    constexpr uint32_t value = 123u;
+    f2 << value;
+  }
+  {
+    serial::IBinaryFile f2("test.txt");
+    uint32_t result = 0;
+    f2 >> result;
+
+    EXPECT_EQ(result, 123u);
+  }
+}
+
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
