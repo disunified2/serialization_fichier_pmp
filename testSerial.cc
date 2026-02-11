b47e59a93db06a366a835042156828a0b420da57
boustiflex <verdxytb@gmail.com>
2026-02-11 21:56:58 +0100

    Ajout de l'implementation de operator>> pour int8_t et uint8_t (à tester)

--- testSerial.cc
+++ testSerial.cc
@@ -8,6 +8,17 @@ TEST(SerialTest, firstTest) {
 
 }
 
+TEST(IBinaryFileOperatorTest,uint8_tOperator) {
+  /*
+  uint8_t x = 0;
+
+  serial::IBinaryFile f("test.bin");
+  f >> x;
+
+  EXPECT_EQ(x, 42);
+  */
+}
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
